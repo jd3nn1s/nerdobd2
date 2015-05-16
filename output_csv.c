73d962d0644e732ddb422d90361799a693fe68d2
jd3nn1s <jd3nn1s@github>
2015-05-16 06:26:20 -0400

    Add CSV output plugin implementation that follows the format recommended by Race Render

--- output_csv.c
+++ output_csv.c
@@ -1,10 +1,57 @@
 #include "plugins.h"
 #include "common.h"
 
-void output_csv_handle_data(obd_data_t *obd) {
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+
+// Use fields recommended by RaceRender at http://racerender.com/Developer/DataFormat.html
+
+static FILE* csv_fh;
+
+void output_double(double val, bool delimiter) {
+    char buf[128];
+    snprintf(buf, sizeof(buf), "%f", val);
+    csv_fwrite(csv_fh, buf, strlen(buf));
+    if (delimiter)
+        fputc(',', csv_fh);
 }
 
-void output_csv_close(void* data) {
+void output_csv_handle_data(obd_data_t *obd) {
+    struct timeval tv;
+    double time_val;
+
+    gettimeofday(&tv, NULL);
+    time_val = tv.tv_sec + ((double)tv.tv_usec / 1000000);
+    output_double(time_val, true);
+#ifdef GPSD_FOUND
+    output_double(obd->gps.longitude, true);
+    output_double(obd->gps.latitude, true);
+    output_double(obd->gps.altitude, true);
+    fprintf(csv_fh, ",,");    // Skip g-force values
+#else
+    fprintf(csv_fh, ",,,,,");
+#endif
+    output_double(obd->speed, true);
+#ifdef GPSD_FOUND
+    output_double(obd->gps.track, true);
+    fputc(',', csv_fh);    // Skip lap
+#else
+    fprintf(csv_fh, ",,");
+#endif
+    output_double(obd->rpm, true);
+    fprintf(csv_fh, ",,");    // Skip gear and throttle
+#ifdef GPSD_FOUND
+    output_double(obd->gps.mode, false);
+#else
+    fputc(',', csv_fh);
+#endif
+    fprintf(csv_fh, "\r\n");
+}
+
+void output_csv_close() {
+    if (csv_fh)
+        fclose(csv_fh);
 }
 
 static struct output_plugin output_csv_plugin = {
@@ -14,5 +61,28 @@ static struct output_plugin output_csv_plugin = {
 
 struct output_plugin* output_plugin_load(void)
 {
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    char buf[128];
+    bool print_header = true;
+
+    snprintf(buf, sizeof(buf), "csv/%02d%02d%02d-%02d%02d00.csv", tm.tm_year + 1900,
+                                                                tm.tm_mon + 1,
+                                                                tm.tm_mday,
+                                                                tm.tm_hour,
+                                                                tm.tm_min);
+
+    if (access(buf, F_OK) == 0)
+        print_header = false;
+
+    csv_fh = fopen(buf, "ab");
+    if (csv_fh == NULL) {
+        fprintf(stderr, "Unable to open %s: ", buf);
+        perror(NULL);
+        return NULL;
+    }
+
+    if (print_header)
+        fprintf(csv_fh, "Time,Longitude,Latitude,Altitude,X,Y,MPH,Heading,Lap,RPM,Gear,Throttle,Accuracy\r\n"); 
     return &output_csv_plugin;
 }
